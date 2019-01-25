54566ed03c713fcfae8945bb49706e2a4eff67dc
Worlanyo <worlanyo@farmerline.co>
2019-01-25 16:30:53 +0000

    Frontend implementation - WIP

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -19,29 +19,30 @@
     <div class="row">
         <div class="col-md-5 col-md-offset-3">
             <p class="meetup-tag-line">Where to go every Friday Night should not be a Problem</p>
-            <!-- <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div> -->
+            
         </div>
     </div>
     <div class="row">
-        <form>
-            <div class="col-md-5 col-md-offset-3">
-            <div class="location-input"><input type="search" placeholder="Enter location"></div>  
-                <div class="meetup-search"> <input type="search" placeholder="I want to go and drink tonight"> </div>
-                <div> <button class="meetup-btn" type="button"> <img src="{{asset('/img/Search.png')}}" alt=""> </button></div>
-            </div>
-        <form>
+        <div class="col-md-5 col-md-offset-3" style="margin-top:20px;">
+                <form>
+                    <div class="inner-form">
+                        <div class="input-field first-wrap">
+                            <input id="location" type="location" placeholder="Enter your location"> 
+                        </div>
+                        <div class="input-field second-wrap">
+                            <input type="search" placeholder="I want to go out for drinks tonight"> 
+                        </div>
+                        <div class="input-field third-wrap">
+                            <button class="meetup-btn" type="button"> <img src="{{asset('/img/Search.png')}}" alt=""> </button>
+                        </div>
+                    </div>
+                    <!-- <div class="col-md-5 col-md-offset-3">
+                    <div class="location-input"><input type="search" placeholder="Enter location"></div>  
+                        <div class="meetup-search"> <input type="search" placeholder="I want to go and drink tonight"> </div>
+                        <div> <button class="meetup-btn" type="button"> <img src="{{asset('/img/Search.png')}}" alt=""> </button></div>
+                    </div> -->
+                <form>
+        </div>        
     </div>
     <div class="row">
          <div class="col-md-12">
@@ -49,4 +50,17 @@
          </div>
     </div>
 </div>
+@endsection
+
+@section('scripts')
+<script src="http://maps.googleapis.com/maps/api/js?key=AIzaSyD9z7CuLsJhxzDVd7Kw494oQlb4TBeYq_Y&libraries=places"></script>
+<script src="/js/jquery.geocomplete.js"></script>
+
+<script type="text/javascript">
+   $(document).ready(function(){
+       // https://github.com/ubilabs/geocomplete
+       $("#location").geocomplete();
+   });
+</script>
+
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -81,5 +81,8 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+  crossorigin="anonymous"></script>
+    @yield('scripts')
 </body>
 </html>
